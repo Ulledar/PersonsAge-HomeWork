bd3fde3817d79628ebd2640b61412ad0b00b3d12
Alex G <ulledar@ua.fm>
2021-05-14 14:46:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace ClassesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Alice", "Johnson", new DateTime(1990, 1, 1), Gender.Female);
+            WriteLine(person.ToString());
+            WriteLine("An now she is " + person.Age + " years old");
+            person.AdoptPet("Baron");
+            person.AdoptPet("Murchik");
+            WriteLine(person.ToString() + " Does she have pets : " + (person.HasPets ? "Yes" : "No"));
+            WriteLine(person.GetPetsName());
+        }
+    }
+}
